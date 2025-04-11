0eb34f17022f42d84b4c1dfa501867d61294550e
Daniil Morozov <steffany20188@mail.ru>
2025-04-11 23:31:16 +0300

    first commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,22 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
+const  cardPlace = document.querySelector(".places__list");
+function addCard(dataCard, deleteCard) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const removeButton = cardElement.querySelector(".card__delete-button");
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardText = cardElement.querySelector(".card__title");
+  cardImage.src = dataCard.link;
+  cardImage.alt = dataCard.name;
+  cardText.textContent = dataCard.name;
+  removeButton.addEventListener("click", deleteCard);
+  return cardElement;
+}
 
-// @todo: DOM узлы
+function removeCard(evt) {
+  evt.target.closest(".card").remove();
+}
 
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+initialCards.forEach(function (item) {
+  const cardElement = addCard(item, removeCard);
+  cardPlace.append(cardElement);
+});
